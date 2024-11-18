be19431053a550f0414202f64f51e2b752cff826
Marouaerraoui <erraouimaroua077@gmail.com>
2024-11-18 16:33:43 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,180 @@
+//Ex1(1ere méthode)
+// function X(){
+//     console.log("bonjour,l monde!");
+// }
+// X();
+//Ex1 (2eme Méthode)
+// function X(){
+//     return "bonjour,le Monde!"
+// }
+// console.log(X());
+//Ex2
+// function add(a,c){
+//     return a+c;
+// }
+// console.log(add(6,7));
+//Ex3
+// function lon (str){
+//     return str.length
+// }
+// console.log(lon("university"));
+//Ex5
+ //function inver(str){
+  //  return str.split("").reverse().join("");
+// }
+//console.log(inver("salut"))
+//Ex4
+//function chaine(str){
+//return  str[0]
+//}
+//console.log(chaine("Maroua"));
+//Ex6
+//function chaine(str){
+   // return str.toUpperCase()
+//}
+//console.log(chaine("sanaa"))
+//Ex7
+// function tab(str){
+//     return str.split(" ")
+// }
+// console.log(tab("pas de problemme"))
+
+//Ex8
+//function resultat(numb){
+   // if ( numb % 2===0 ){
+   //     return "pair"
+   // } else{
+
+   // }
+   //     return "impair"
+
+    //}
+//console.log(resultat(15))
+//Ex
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let person={ nom: "Alice", âge: 25 };
+// for(let  propriété in person ){
+//     console.log( propriété +": " + person[ propriété] )
+// }
+
+//Ex7:
+//  let fruits=["APPLE","BANANA","CHERRY"]
+//  fruits.forEach(function (fruits){
+//      console.log(fruits)
+//  })
+//Ex6:
+//  let num=1
+//  for(i=1;i<=5;i++){
+//      num *=i
+//      console.log(num)
+//  }
+//Ex10:
+//  let sum=8
+//  for(let i=1;i<10;i++){
+//     if(sum >=30){
+//          console.log("limit reached!")
+//      }else{
+//          sum += i;
+         
+//      }
+//  }
+
+//Ex5:
+//Afficher les nombre impaire
+// let num=15
+// console.log("impaire")
+// for(let i=1;i<=15;i++){
+//    if (i % 2!==0){
+//     console.log(i)
+//    }
+// }
+
+//afficher les nombre paire
+// console.log("pair")
+// for(let i=1;i<=15;i++){
+//     if(i % 2===0){
+//         console.log(i)
+//     }
+// }
+//Ex4:
+// let couleur= ["rouge", "vert", "bleu"]
+// couleur.forEach(function (couleur){
+//     console.log(couleur)
+// })
+    
+//Ex3:
+// let num=5
+// while(num>=1){
+//     console.log(num);
+//     num--;
+// 
+//Ex1:
+// let num=0
+// for(let i=0; i<10; i++){
+//     console.log(i)
+// }
+
+
+
+// let age=34
+// age>=18?console.log("can drive"):console.log("cannot drive!")
+
+// let year=2024
+// if(year<=2024){
+//     console.log("a leap year")
+// }else{
+//     console.log("is not a leap year!")
+// }
+
+
+// let light="red"
+// switch(light){
+//     case "red":
+//         console.log("Stop")
+//         break;
+//         case "yellow":
+//             console.log("Slow down")
+//             break;
+//             case "green":
+//                 console.log("Go")
+//                 break;
+//                 default:
+//                     console.log("is not light!")
+//                     break;
+// }         
+
+
+
+
+// let age=65
+// if(age<13){
+//     console.log("Enfant")
+// }else if(age>13 && age<=17){
+//     console.log("adolescent")
+// }else{
+//     console.log("Adulte")
+// }
+
+
+
