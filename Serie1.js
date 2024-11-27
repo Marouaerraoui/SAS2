c8eee422bc42e3e8593ab83eb249c31acc4b8e0b
Marouaerraoui <erraouimaroua077@gmail.com>
2024-11-27 12:24:31 +0100

    first navbar

--- Serie1.js
+++ Serie1.js
@@ -1,2 +1,14 @@
-let a="pas de probleme"
-console.log(a);
+// function num(str){
+//     return str.charAt(0);
+// }
+// console.log(num("SANAA"));
+
+
+// const num=(number)=>{
+//     if(number%2==0){
+//         return console.log("le nombre est :" + number + "paire")
+//     }else{
+//         return  console.log("le nombre est :" + number + "impaire")
+//     }
+// }
+// num(13);
